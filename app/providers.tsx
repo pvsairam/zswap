b7c027644b3a51ad379723d445ca3793ba144180
Sairam <pvsairam@hotmail.com>
2025-11-16 16:38:35 +0530

    Replace MetaMaskEthersSignerProvider with UnifiedWallet

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,8 +4,9 @@ import type { ReactNode } from "react";
 
 import { MetaMaskProvider } from "@/core/metamask/useMetaMaskProvider";
 import { InMemoryStorageProvider } from "@/core/useInMemoryStorage";
-import { MetaMaskEthersSignerProvider } from "@/core/metamask/useMetaMaskEthersSigner";
 import { TelegramProvider } from "@/core/useTelegram";
+import { UnifiedWalletProvider } from "@/core/wallet/useUnifiedWallet";
+import { UnifiedWalletSignerProvider } from "@/core/wallet/useUnifiedWalletSigner";
 
 type Props = {
   children: ReactNode;
@@ -15,9 +16,11 @@ export function Providers({ children }: Props) {
   return (
     <TelegramProvider>
       <MetaMaskProvider>
-        <MetaMaskEthersSignerProvider initialMockChains={{ 31337: "http://localhost:8545" }}>
-          <InMemoryStorageProvider>{children}</InMemoryStorageProvider>
-        </MetaMaskEthersSignerProvider>
+        <UnifiedWalletProvider>
+          <UnifiedWalletSignerProvider>
+            <InMemoryStorageProvider>{children}</InMemoryStorageProvider>
+          </UnifiedWalletSignerProvider>
+        </UnifiedWalletProvider>
       </MetaMaskProvider>
     </TelegramProvider>
   );
